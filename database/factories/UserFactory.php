450e5bf3336da8a7634be21b036b55dffa546e7d
Unknown <edson.ruvalcaba@alumnos.udg.mx>
2019-07-20 21:24:04 -0500

    adding fields user

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,10 +18,13 @@ use Faker\Generator as Faker;
 
 $factory->define(User::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
+        'name' => $faker->firstName,
+		  'lastN' => $faker->lastName,
+		  'alias' => $faker->randomElement(['','Lic. ','','Ing. ','','Arq. ']).$faker->firstName.' '.$faker->lastName,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'password' => bcrypt('123456'), // password
+        // 'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
     ];
 });
