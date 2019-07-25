5d322e8fe5117557bc74b49af774b57fbb004aae
home <edson.ruvalcaba@alumnos.udg.mx>
2019-07-24 22:27:35 -0500

    make:md -a tienda

---
+++ database/factories/TiendaFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Tienda;
+use Faker\Generator as Faker;
+
+$factory->define(Tienda::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
