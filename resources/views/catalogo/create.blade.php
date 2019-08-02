194390f69f6afb979e784954ffe9f92c520c431c
home <edson.ruvalcaba@alumnos.udg.mx>
2019-08-01 19:35:15 -0500

    catalogo module complete

---
+++ resources/views/catalogo/create.blade.php
@@ -0,0 +1,69 @@
+@extends('layouts.app2')
+@section('content')
+	<div class="container">
+		<div class="col-md-8 offset-md-2 col-sm-12">
+			<div class="card">
+				<div class="card-header text-center bg-light" style="font-size:1.2em;">
+					Agregar Catalogo
+				</div>
+				<div class="card-body">
+					<form class="" action="{{ route('catalogos.store')}}" method="post" enctype="multipart/form-data">
+						@csrf
+						<div class="form-group">
+							<label for="nombre">Nombre</label>
+							<input type="text" class="form-control" name="nombre" id="nombre" placeholder="">
+							{{-- <p class="help-block">Help text here.</p> --}}
+						</div>
+						<div class="form-group">
+							<label for="temporada">Temporada</label>
+							<input type="text" class="form-control" name="temporada" id="temporada" placeholder="">
+							{{-- <p class="help-block">Help text here.</p> --}}
+						</div>
+						<div class="form-group">
+							<label for="year">Año</label>
+							<select class="form-control" name="year" id="year">
+								{{-- <option value="2018">2018</option>
+								<option selected value="2019">2019</option>
+								<option value="2020">2020</option>
+								<option value="2021">2021</option> --}}
+								@foreach ($years as $y)
+									@if ($y == now()->year)
+										<option selected value="{{$y}}">{{ $y }}</option>
+									@endif
+									<option value="{{$y}}">{{ $y }}</option>
+								@endforeach
+							</select>
+							{{-- <p class="help-block">Help text here.</p> --}}
+						</div>
+						<div class="form-group">
+							<label for="tienda">Tienda</label>
+							<select class="form-control" name="tienda" id="tienda">
+								<option selected disabled>Selecciona una Tienda/Proveedor</option>
+								@foreach ($tiendas as $tienda)
+									<option value="{{ $tienda->id }}">{{ $tienda->name }}</option>
+								@endforeach
+							</select>
+							{{-- <p class="help-block">Help text here.</p> --}}
+						</div>
+
+						<div class="form-group">
+							<label for="portada">Portada del catalogo</label>
+							<input type="file" class="form-control-file" name="portada" id="portada" placeholder="">
+							{{-- <p class="help-block">Help text here.</p> --}}
+						</div>
+						<div class="form-group text-center ">
+							<input type="submit" class="col-xs-12 col-md-6 btn btn-primary"value="Guardar">
+						</div>
+					</form>
+				</div>
+			</div>
+			@if ($errors->any())
+				<ul class="list-unstyled alert alert-danger" role="alert">
+					@foreach ($errors->all() as $error)
+						<li><i class="fas fa-exclamation-circle"></i> {{$error}}</li>
+					@endforeach
+				</ul>
+			@endif
+		</div>
+	</div>
+@endsection
---
+++ resources/views/catalogo/index.blade.php
@@ -0,0 +1,157 @@
+@extends('layouts.app2')
+
+@section('content')
+<div class="container">
+	<div class="card">
+		<div class="card-header">
+			<div class="float-left mt-2">
+				<h5>Catalogos</h5>
+			</div>
+			<div class="float-right">
+				<a href="{{ route('catalogos.create')}}" class=" btn btn-primary"><i class="fas fa-plus"></i>	</a>
+			</div>
+
+		</div>
+		@if ($catalogos->isEmpty())
+			<div class="bg-secondary text-white">
+				<h4 class="text-center mt-2">No hay catalogos registradas</h4>
+			</div>
+		@else
+			<table class="table card-table table-responsive-sm">
+				<thead class="thead-dark">
+					<tr>
+						<th scope="col" style="">Catalogo</th>
+						<th scope="col" style="">Temporada</th>
+						<th scope="col" style="">Año</th>
+						<th scope="col" style="">Tienda</th>
+						<th scope="col">Acciones</th>
+					</tr>
+				</thead>
+				<tbody>
+					@foreach ($catalogos as $catalogo)
+						<tr>
+							<td>{{ $catalogo->nombre }}</td>
+							<td>{{ $catalogo->temporada }}</td>
+							<td>{{ $catalogo->year }}</td>
+							<td>{{ $catalogo->tienda }}</td>
+							<td>
+								<div class="btn-group btn-group-sm" role="group" aria-label="actions" idi={{ $catalogo->id }}>
+									{{-- <button type="button" class="btn btn-outline-dark"><i class="fas fa-eye"></i></button> --}}
+									<a href="{{ route('catalogos.edit',$catalogo->id) }}" class="btn btn-primary"><i class="fas fa-pencil-alt"></i></a>
+									<button type="button" class="btn btn-danger" data-toggle="modal" data-target="#delModal"><i class="fas fa-trash-alt"></i></button>
+								</div>
+							</td>
+						</tr>
+					@endforeach
+				</tbody>
+			</table>
+			<div class="modal fade" id="delModal" tabindex="-1" role="dialog" aria-labelledby="delModalLabel" aria-hidden="true">
+				<div class="modal-dialog" role="document">
+					<div class="modal-content">
+						<div class="modal-header">
+							<h5 class="modal-title" id="exampleModalLabel">Eliminar Catalogo?</h5>
+							<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+								<span aria-hidden="true">&times;</span>
+							</button>
+						</div>
+						<div class="modal-body">
+							<table class="table">
+								<tbody>
+									<tr>
+										<th style="width:20%;" scope="row">Catalogo:</th>
+										<td colspan="3" class="mna"></td>
+									</tr>
+									<tr>
+										<th style="width:20%;" scope="row">Temporada:</th>
+										<td colspan="3" class="mtm"></td>
+									</tr>
+									<tr>
+										<th style="width:20%;" scope="row">Tienda:</th>
+										<td class="mst"></td>
+										<th style="width:20%;" scope="row">Año:</th>
+										<td class="mye"></td>
+									</tr>
+									<tr class="tri">
+										<td colspan="4" class="text-center">
+											<img src="" alt="" class="img-fluid">
+										</td>
+									</tr>
+								</tbody>
+							</table>
+						</div>
+						<div class="modal-footer">
+							<form action="/catalogos/" method="post">
+								@csrf
+								@method('DELETE')
+								<input class="btn btn-danger" type="submit" value="Eliminar">
+							</form>
+							<button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		@endif
+	</div>
+</div>
+<script type="text/javascript">
+	$(document).ready(function() {
+		$('.fa-eye').parent().click(function(event) {
+			var id = $(this).parent().attr('idi');
+			$.ajax({
+				url: '/catalogos/'+id,
+				type: 'GET',
+			})
+			.done(function(x) {
+
+				console.log(x);
+				console.log("success");
+			})
+			.fail(function(x) {
+				console.log(x);
+				console.log("error");
+			})
+			.always(function() {
+				console.log("complete");
+			});
+		});
+
+		$('.fa-trash-alt').parent().click(function(event) {
+			$('#delModal .tri').hide();
+			var id = $(this).parent().attr('idi');
+			$.ajax({
+				url: '/catalogos/'+id,
+				type: 'GET',
+			})
+			.done(function(c) {
+				console.log(c);
+				$('#delModal .mna').append(c.nombre);
+				$('#delModal .mtm').append(c.temporada);
+				$('#delModal .mst').append(c.tienda);
+				$('#delModal .mye').append(c.year);
+				if (c.portada) {
+					$('#delModal .tri').show();
+					$('#delModal .tri img').attr('src', '/images/catalogos/'+c.portada);
+					$('#delModal .tri img').attr('alt', c.portada);
+				}
+				$('#delModal form').attr('action', '/catalogos/'+c.id);
+				// console.log(t);
+				// console.log("success");
+			})
+			.fail(function(c) {
+				$('#delModal .mna').append('Error');
+				$('#delModal .mtm').append('Error');
+				$('#delModal .mst').append('Error');
+				$('#delModal .mye').append('Error');
+				console.log(c);
+			});
+		});
+		$('#delModal').on('hidden.bs.modal', function(e) {
+			$('#delModal .mna').text('');
+			$('#delModal .mtm').text('');
+			$('#delModal .mst').text('');
+			$('#delModal .mye').text('');
+		});
+
+	});
+</script>
+@endsection
