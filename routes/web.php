78b7dc03c214a45a1de4add4d5207ef70fd84239
home <edson.ruvalcaba@alumnos.udg.mx>
2019-08-13 14:22:35 -0500

    manually added ubicacion routes

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,26 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes(['register' => false]);
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/home', 'HomeController@index')->name('home');
+
+// Route::resource('ubicaciones','UbicacionController');
+// Route::resource('ubicaciones','UbicacionController');
+Route::get('/ubicaciones','UbicacionController@index')->name('ubicaciones.index');
+Route::get('/ubicaciones/nueva','UbicacionController@create')->name('ubicaciones.nueva');
+Route::get('/ubicaciones/create','UbicacionController@create')->name('ubicaciones.create');
+Route::get('/ubicaciones/{ubicacion}','UbicacionController@show')->name('ubicaciones.show');
+Route::get('/ubicaciones/{ubicacion}/edit','UbicacionController@edit')->name('ubicaciones.edit');
+Route::post('/ubicaciones','UbicacionController@store')->name('ubicaciones.store');
+Route::put('/ubicaciones/{ubicacion}','UbicacionController@update')->name('ubicaciones.update');
+Route::delete('/ubicaciones/{ubicacion}','UbicacionController@destroy')->name('ubicaciones.destroy');
+
+Route::resource('catalogos','CatalogoController');
+Route::resource('tienda','TiendaController');
+
+// Route::get('usuarios/rol/{role}','UserController@usrole')->name('usuarios.usrole');
+Route::resource('usuarios','UserController');
+
+
+Route::get('logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');
+Auth::routes(['register' => false]);
