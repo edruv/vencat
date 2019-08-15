a535b78d39f5097eddac69ffefe47579124d29a7
home <edson.ruvalcaba@alumnos.udg.mx>
2019-08-14 20:51:43 -0500

    added basic fields to migration

--- database/migrations/2019_08_13_141430_create_pedidos_table.php
+++ database/migrations/2019_08_13_141430_create_pedidos_table.php
@@ -6,26 +6,37 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreatePedidosTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('pedidos', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->timestamps();
-        });
-    }
+	/**
+	* Run the migrations.
+	*
+	* @return void
+	*/
+	public function up()
+	{
+		Schema::create('pedidos', function (Blueprint $table) {
+			$table->bigIncrements('id');
+			$table->string('product')->nullable();
+			$table->string('model');
+			$table->string('color')->nullable();
+			$table->string('size')->nullable();
+			$table->float('costo',8,2);
+			$table->integer('page');
+			$table->unsignedBigInteger('customer');
+			$table->unsignedBigInteger('taken');
+			$table->foreign('customer')->references('id')->on('users');
+			$table->foreign('taken')->references('id')->on('users');
+			$table->SoftDeletes();
+			$table->timestamps();
+		});
+	}
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('pedidos');
-    }
+	/**
+	* Reverse the migrations.
+	*
+	* @return void
+	*/
+	public function down()
+	{
+		Schema::dropIfExists('pedidos');
+	}
 }
