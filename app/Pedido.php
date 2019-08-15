ec0dbb5bf50493fef8a999a3e8580b90705baf35
home <edson.ruvalcaba@alumnos.udg.mx>
2019-08-14 20:53:39 -0500

    softDeletes & fields filables

--- app/Pedido.php
+++ app/Pedido.php
@@ -3,8 +3,18 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Pedido extends Model
 {
-    //
+	use SoftDeletes;
+
+	/**
+	* The attributes that are mass assignable.
+	*
+	* @var array
+	*/
+	protected $fillable = [
+		'product','model','color','size','costo','page'
+	];
 }
